f3b5e4cdbc94b89bc6296faafddc3b99cc19f18c
Leonardo Rossi <83621860+leorossiio@users.noreply.github.com>
2025-04-06 17:16:01 -0300

    CLINIXSM-5 :: inicialização do projeto angular e estruturação inicial

---
+++ clinix/src/app/cadastro-paciente/cadastro-paciente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-paciente',
+  imports: [],
+  templateUrl: './cadastro-paciente.component.html',
+  styleUrl: './cadastro-paciente.component.css'
+})
+export class CadastroPacienteComponent {
+
+}
