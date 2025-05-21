40684702cdccba40342ef8ba7c3998f744cfecb8
Guilherme <gmoraes806@gmail.com>
2025-05-20 23:52:13 -0300

    CLINIXSM-31 :: tela de atualizacao de dados do usuario

--- clinix/src/app/features/usuarios/components/modal-atualizacao-usuario/modal-atualizacao-usuario.component.ts
+++ clinix/src/app/features/usuarios/components/modal-atualizacao-usuario/modal-atualizacao-usuario.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Usuario } from '../../../../models/usuario.model';
 
 @Component({
-  selector: 'app-modal-atualizacao-usuario',
-  imports: [],
+  selector: 'modal-atualizacao-usuario',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './modal-atualizacao-usuario.component.html',
-  styleUrl: './modal-atualizacao-usuario.component.css'
+  styleUrls: ['./modal-atualizacao-usuario.component.css']
 })
 export class ModalAtualizacaoUsuarioComponent {
+  @Input() usuario: Usuario | null = null;
+  @Output() fechar = new EventEmitter<void>();
+  @Output() salvar = new EventEmitter<Usuario>();
 
+  salvarAlteracoes(): void {
+    if (this.usuario) {
+      this.salvar.emit(this.usuario);
+      this.fechar.emit();
+    }
+  }
 }
